a4626b39147951cad4addb1cc1cd00ff66d36b63
Maxime <maxime.sciare@gmail.com>
2023-05-21 09:31:40 +0200

    Updated profile page

--- lib/data/profile_data.dart
+++ lib/data/profile_data.dart
@@ -7,7 +7,7 @@ class ProfileData {
       "Cineflix is a mobile app based on Flutter. It allows the user to search movies and save them as favorites.",
       "On each film page, the user can watch the trailer directly or get redirected on Youtube and share the movie on social medias and messaging apps.",
       "The user can also mark the film as watched or unwatched.",
-      "An AI model is embedded into the app, making suggestions to the user based on its preferences (= favorites) and popular movies."
+      "A recommandation model is available in the app, making suggestions to the user based on its preferences (= favorites) and popular movies."
     ]),
     ProfileFacts(title: "Thanks", facts: [
       "I would like to thanks Julien Ratouit for giving me 5 must-watch movies for the app selection."
